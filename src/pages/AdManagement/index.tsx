fa29f425f4b016b1b302e7b311279d2ae4a1b9fa
malgamlee <malgamlee@gmail.com>
2022-05-24 23:45:02 +0900

    fix. add managementItem

--- src/pages/AdManagement/index.tsx
+++ src/pages/AdManagement/index.tsx
@@ -6,7 +6,24 @@ import { adStatusSelectState } from 'states/Atoms';
 import useRecoil from 'hooks/useRecoil';
 import styles from './adManagement.module.scss';
 
+import ManagementItem from './managementItem';
+
 const SHOW_STATUS = ['전체 광고', '진행중', '중단됨'];
+
+export interface IProps {
+  id: number;
+  adType: string;
+  title: string;
+  budget: number;
+  status: string;
+  startDate: string;
+  endDate: string | null;
+  report: {
+    cost: number;
+    convValue: number;
+    roas: number;
+  };
+}
 
 function AdManagement() {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,81 +38,14 @@ function AdManagement() {
     setIsOpen((prev) => !prev);
   };
 
-  const handleUpdateAdManagement = (e: MouseEvent<HTMLButtonElement>) => {
-    if (e.currentTarget.dataset.value === 'create') {
-      console.log(e);
-    }
-    console.log(e);
-  };
-
-  const convertMoney = (money: number) => {
-    const tmpNum = Math.floor(money / 1000);
-    if (String(money).length > 5) {
-      const tmpNum2 = Math.floor(tmpNum / 10)
-        .toString()
-        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-      if (tmpNum % 10 === 0) return `${tmpNum2}만`;
-      return `${tmpNum2}만 ${tmpNum % 10}천`;
-    }
-    return `${tmpNum}천`;
-  };
-
   const cardStructure = AdListData.ads
     .filter((data) => {
       if (selectStatus === '진행중') return data.status === 'active';
       if (selectStatus === '중단됨') return data.status === 'ended';
       return data;
     })
-    .map((item) => {
-      return (
-        <div className={styles.adCard} key={item.id}>
-          <p>
-            {item.adType === 'web' ? '웹광고' : '앱광고'}_{item.title}
-          </p>
-          <ul>
-            <li>
-              <dt className={styles.title}>상태</dt>
-              <dl className={styles.content}>
-                {item.status === 'active' ? '진행중' : '중단됨'}
-              </dl>
-            </li>
-            <li>
-              <dt className={styles.title}>광고 생성일</dt>
-              <dl className={styles.content}>
-                {item.startDate.split('T')[0]}{' '}
-                {item.endDate ? `(${item.endDate.split('T')[0]})` : ''}
-              </dl>
-            </li>
-            <li>
-              <dt className={styles.title}>일 희망 예산</dt>
-              <dl className={styles.content}>{convertMoney(item.budget)}원</dl>
-            </li>
-            <li>
-              <dt className={styles.title}>광고 수익률</dt>
-              <dl className={styles.content}>{item.report.roas}%</dl>
-            </li>
-            <li>
-              <dt className={styles.title}>매출</dt>
-              <dl className={styles.content}>
-                {convertMoney(item.report.convValue)}원
-              </dl>
-            </li>
-            <li>
-              <dt className={styles.title}>광고 비용</dt>
-              <dl className={styles.content}>
-                {convertMoney(item.report.cost)}원
-              </dl>
-            </li>
-          </ul>
-          <button
-            type="button"
-            className={styles.btnModify}
-            data-value={item.id}
-          >
-            수정하기
-          </button>
-        </div>
-      );
+    .map((manageItem) => {
+      return <ManagementItem {...manageItem} key={manageItem.id} />;
     });
 
   return (
@@ -130,12 +80,7 @@ function AdManagement() {
             })}
           </ul>
         </div>
-        <button
-          type="button"
-          className={styles.btnMakeAd}
-          onClick={handleUpdateAdManagement}
-          data-value="create"
-        >
+        <button type="button" className={styles.btnMakeAd} data-value="create">
           광고 만들기
         </button>
       </div>
