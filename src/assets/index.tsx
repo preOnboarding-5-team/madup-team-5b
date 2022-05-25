ff924a16fcf80fab9d32c33ced5882507983f80b
Moon-Ga <dev.jaeseok@gmail.com>
2022-05-26 03:09:19 +0900

    revert & fix

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,9 +1,3 @@
-import { ReactComponent as ArrowDownIcon } from './svgs/arrow-down-icon.svg';
-import { ReactComponent as CheckIcon } from './svgs/check-icon.svg';
-import { ReactComponent as HigherValueIcon } from './svgs/higher-value-icon.svg';
-import { ReactComponent as LowerValueIcon } from './svgs/lower-value-icon.svg';
-
-export { ArrowDownIcon, CheckIcon, HigherValueIcon, LowerValueIcon };
 import { ReactComponent as LeverLogo } from './svgs/lever-logo.svg';
 import { ReactComponent as ArrowDownIcon } from './svgs/arrow-down-icon.svg';
 import { ReactComponent as DashboardIcon } from './svgs/dashboard-icon.svg';
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,3 @@
-import TrendDataBoard from 'pages/Dashboard/TrendDataBoard';
 import DatePicker from 'components/common/DatePicker';
 import { useRecoilValue } from 'recoil';
 import { refinedDateRangeState } from 'states';
@@ -12,18 +11,11 @@ function Dashboard() {
   console.log(start, end);
   return (
     <div className={styles.dashboard}>
-<<<<<<< HEAD
-      <TrendDataBoard />
-=======
       <div className={styles.titleContainer}>
         <div className={styles.title}>대시보드</div>
         <DatePicker />
       </div>
-<<<<<<< HEAD
       <TrendDataBoard />
-=======
->>>>>>> d4cc662f453c08abba371d74661b04870b8859ac
->>>>>>> 7ed07af7fd9f1570745e9e35c82c76a542caea30
       <MediaChannel />
     </div>
   );
